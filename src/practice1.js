fbdba773e700eb9f0c07abb149a84266ad3755f5
Xiran LYU <xiran.lyu@thoughtworks.com>
2020-10-16 01:02:45 +0800

    tests passed

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,14 @@
-const parseData
+const parseData = ({data, column}) => {
+    let keys = [];
+    column.map((obj) => {
+        keys.push(obj.name)
+    });
+    return data.map((info) => (
+        Object.assign({}, ...(info.map((value, index) => (
+            { [keys[index]]: value }
+            )))
+        )
+        ))
+}
+
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,8 @@
-const inject
+const inject = (items, sections) => {
+    for (let obj of sections) {
+        //sections.indexOf(obj)
+        items.splice(obj.index + sections.findIndex((a) => a === obj), 0, obj.content);
+    }
+    return items;
+}
 export { inject };
